15fc769755733d51c107422e6e000139cdd8bce3
Fernando Warno <ferwar@hotmail.com>
2023-10-06 05:37:17 -0300

    1.-Se da formato al codigo

--- js/contacto.js
+++ js/contacto.js
@@ -1,8 +1,8 @@
 window.addEventListener("pageshow", () => {
-	document.getElementById("name").value = "";
-	document.getElementById("email").value = "";
-	document.getElementById("telefono").value = "";
-	document.getElementById("message").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("telefono").value = "";
+    document.getElementById("message").value = "";
     document.getElementById("nameError").textContent = "";
     document.getElementById("emailError").textContent = "";
     document.getElementById("telefonoError").textContent = "";
@@ -32,15 +32,18 @@ function validarNombre(name) {
         nameError.textContent = "Por favor, introduce tu nombre";
         return false;
     }
+
     const nameRegex = /^[a-zA-Z\s]*$/;
     if (!nameRegex.test(name)) {
         nameError.textContent = "Por favor, introduce un nombre válido. Solo se permiten letras y espacios.";
         return false;
     }
+
     if (name.length < 2 || name.length > 35) {
         nameError.textContent = "Por favor, introduce un nombre válido. Debe tener entre 2 y 35 caracteres.";
         return false;
     }
+
     nameError.textContent = "";
     return true;
 }
@@ -70,7 +73,7 @@ function validarEmail(email) {
 }
 
 function validarTelefono(telefono) {
-	const telefonoError = document.getElementById("telefonoError");
+    const telefonoError = document.getElementById("telefonoError");
     const telefonoRegex = /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/;
 
     telefono = telefono.trim();
@@ -90,7 +93,8 @@ function validarTelefono(telefono) {
 }
 
 function validarConsulta(consulta) {
-	const consultaError = document.getElementById("consultaError");
+    const consultaError = document.getElementById("consultaError");
+    
     if (consulta === "") {
         consultaError.textContent = "Por favor, introduce tu consulta";
         return false;
@@ -106,17 +110,17 @@ function validarConsulta(consulta) {
 }
 
 function validarFormulario(event) {
-	const name = document.getElementById("name").value.trim();
-	const email = document.getElementById("email").value.trim();
-	const telefono = document.getElementById("telefono").value.trim();
-	const consulta = document.getElementById("message").value.trim();
-	
-	if (!validarNombre(name) || !validarEmail(email) || !validarTelefono(telefono) || !validarConsulta(consulta)) {
-		event.preventDefault();
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const telefono = document.getElementById("telefono").value.trim();
+    const consulta = document.getElementById("message").value.trim();
+
+    if (!validarNombre(name) || !validarEmail(email) || !validarTelefono(telefono) || !validarConsulta(consulta)) {
+        event.preventDefault();
         return false;
     }
 
-	return true;
+    return true;
 }
 
 const submitBtn = document.querySelector(".enviar");
